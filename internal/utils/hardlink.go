1b9b65eed6a10b1c73d8f0fbfd8de4cfd833c9fb
nuxen <47067662+nuxencs@users.noreply.github.com>
2023-12-11 22:39:15 +0100

    refactor(utils): changed hardlink error handling

--- internal/utils/hardlink.go
+++ internal/utils/hardlink.go
@@ -1,34 +1,22 @@
 package utils
 
 import (
-	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
-
-	"seasonpackarr/pkg/errors"
 )
 
 func CreateHardlink(srcPath, trgPath string) error {
-	// create the target directory if it doesn't exist
 	trgDir := filepath.Dir(trgPath)
-	err := os.MkdirAll(trgDir, 0755)
-	if err != nil {
+
+	// create the target directory if it doesn't exist
+	if err := os.MkdirAll(trgDir, 0755); err != nil {
 		return err
 	}
 
-	if _, err = os.Stat(trgPath); err == nil {
-		// target file exist
-		return fmt.Errorf("file already exist")
-	} else if errors.Is(err, fs.ErrNotExist) {
-		// target file does not exist, create a hardlink
-		err = os.Link(srcPath, trgPath)
-		if err != nil {
-			return err
-		}
-	} else {
-		// any other error
+	// link source path to target path if it doesn't exist
+	if err := os.Link(srcPath, trgPath); err != nil {
 		return err
 	}
+
 	return nil
 }
