f626ac1d365b66dc504a2ae046a6dea3996fe18f
nuxen <theonenuxen@proton.me>
2024-01-20 14:14:08 +0100

    chore(tests): adjust formatting tests

--- internal/utils/formatting_test.go
+++ internal/utils/formatting_test.go
@@ -20,72 +20,72 @@ func Test_GetFormattedTitle(t *testing.T) {
 		{
 			name:     "pack_1",
 			packName: "Prehistoric Planet 2022 S02 1080p ATVP WEB-DL DDP 5.1 Atmos H.264-FLUX",
-			want:     "prehistoric planet202221080pweb-dlfluxatvp",
+			want:     "prehistoric planet20222",
 		},
 		{
 			name:     "pack_2",
 			packName: "Rabbit Hole S01 1080p AMZN WEB-DL DDP 5.1 H.264-NTb",
-			want:     "rabbit hole011080pweb-dlntbamzn",
+			want:     "rabbit hole01",
 		},
 		{
 			name:     "pack_3",
 			packName: "Star Wars Visions S01 REPACK 1080p DSNP WEB-DL DDP 5.1 H.264-FLUX",
-			want:     "star wars visions011080pweb-dlfluxrepackdsnp",
+			want:     "star wars visions01",
 		},
 		{
 			name:     "pack_4",
 			packName: "Star Wars Visions S02 1080p DSNP WEB-DL DDP 5.1 H.264-NTb",
-			want:     "star wars visions021080pweb-dlntbdsnp",
+			want:     "star wars visions02",
 		},
 		{
 			name:     "pack_5",
 			packName: "The Good Doctor S06 1080p AMZN WEB-DL DDP 5.1 H.264-NTb",
-			want:     "the good doctor061080pweb-dlntbamzn",
+			want:     "the good doctor06",
 		},
 		{
 			name:     "pack_6",
 			packName: "The Good Doctor S06 REPACK 1080p AMZN WEB-DL DDP 5.1 H.264-NTb",
-			want:     "the good doctor061080pweb-dlntbamzn",
+			want:     "the good doctor06",
 		},
 		{
 			name:     "pack_7",
 			packName: "The Mandalorian S03 1080p DSNP WEB-DL DDP 5.1 Atmos H.264-FLUX",
-			want:     "the mandalorian031080pweb-dlfluxdsnp",
+			want:     "the mandalorian03",
 		},
 		{
 			name:     "pack_8",
 			packName: "Gold Rush: White Water S06 1080p AMZN WEB-DL DDP 2.0 H.264-NTb",
-			want:     "gold rush white water061080pweb-dlntbamzn",
+			want:     "gold rush white water06",
 		},
 		{
 			name:     "pack_9",
 			packName: "Transplant S03 1080p iT WEB-DL AAC 2.0 H.264-NTb",
-			want:     "transplant031080pweb-dlntbit",
+			want:     "transplant03",
 		},
 		{
 			name:     "pack_10",
 			packName: "Mayans M.C. S05 1080p AMZN WEB-DL DDP 5.1 H.264-NTb",
-			want:     "mayans m c051080pweb-dlntbamzn",
+			want:     "mayans m c05",
 		},
 		{
 			name:     "pack_11",
 			packName: "What If... S01 1080p DNSP WEB-DL DDP 5.1 H.264-FLUX",
-			want:     "what if011080pweb-dlfluxdnsp",
+			want:     "what if01",
 		},
 		{
 			name:     "pack_12",
 			packName: "Demon Slayer Kimetsu no Yaiba S04 2023 1080p WEB-DL AVC AAC 2.0 Dual Audio -ZR-",
-			want:     "demon slayer kimetsu no yaiba202341080pweb-dlzr",
+			want:     "demon slayer kimetsu no yaiba20234",
 		},
 		{
 			name:     "pack_13",
 			packName: "The Continental 2023 S01 2160p PCOK WEB-DL DDP5.1 Atmos DV HDR H.265-FLUX",
-			want:     "the continental202312160pweb-dlfluxdvhdrpcok",
+			want:     "the continental20231",
 		},
 		{
 			name:     "pack_14",
 			packName: "The Continental 2023 S01 2160p PCOK WEB-DL DDP5.1 Atmos HDR DV H.265-FLUX",
-			want:     "the continental202312160pweb-dlfluxdvhdrpcok",
+			want:     "the continental20231",
 		},
 	}
 	for _, tt := range tests {
