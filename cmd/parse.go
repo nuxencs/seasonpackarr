375577aa7429313ca4cc8281801988d356a711fd
nuxen <theonenuxen@proton.me>
2024-04-12 19:09:51 +0200

    chore(cmd): indent test command examples

--- cmd/parse.go
+++ cmd/parse.go
@@ -16,7 +16,7 @@ import (
 var parseCmd = &cobra.Command{
 	Use:     "parse",
 	Short:   "Test the parse api endpoint for a specified release",
-	Example: `seasonpackarr test parse --rls “Series.S01.1080p.WEB-DL.H.264-RlsGrp” --client "default" --host "127.0.0.1" --port 42069 --api "your-api-key"`,
+	Example: `  seasonpackarr test parse --rls “Series.S01.1080p.WEB-DL.H.264-RlsGrp” --client "default" --host "127.0.0.1" --port 42069 --api "your-api-key"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		torrentBytes, err := torrents.TorrentFromRls(rlsName, 5)
 		if err != nil {
