c38131bb9ea2991e9dce872d3f4875aac935a0b0
nuxen <theonenuxen@proton.me>
2024-10-01 18:13:55 +0200

    fix(processor): wrong response codes for matches

--- internal/http/webhook.go
+++ internal/http/webhook.go
@@ -4,8 +4,6 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/nuxencs/seasonpackarr/internal/config"
 	"github.com/nuxencs/seasonpackarr/internal/domain"
 	"github.com/nuxencs/seasonpackarr/internal/logger"
@@ -34,10 +32,8 @@ func (h *webhookHandler) Routes(r *gin.RouterGroup) {
 
 func (h *webhookHandler) pack(c *gin.Context) {
 	newProcessor(h.log, h.cfg, h.noti).ProcessSeasonPackHandler(c.Writer, c.Request)
-	c.Status(http.StatusOK)
 }
 
 func (h *webhookHandler) parse(c *gin.Context) {
 	newProcessor(h.log, h.cfg, h.noti).ParseTorrentHandler(c.Writer, c.Request)
-	c.Status(http.StatusOK)
 }
